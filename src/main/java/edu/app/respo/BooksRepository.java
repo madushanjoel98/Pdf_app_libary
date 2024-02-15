e3312675df8e85b574c18a389aee72b11b0439ba
madushanjoel <madushanjoel@gmail.com>
2024-02-15 10:49:30 +0530

    mapping the post

--- src/main/java/edu/app/respo/BooksRepository.java
+++ src/main/java/edu/app/respo/BooksRepository.java
@@ -13,7 +13,7 @@ public interface BooksRepository
 extends JpaRepository<Books, Long> {
     List<Books> findBySubcategory_Sutid(int sutid);
 
-    @Query("select b from Books b where b.book_name like ?1")
+    @Query("select b from Books b where b.book_name like %?1%")
     List<Books> findByBook_nameLike(String book_name);
 
 
